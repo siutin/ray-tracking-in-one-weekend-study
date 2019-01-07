c6e306baf05ad4993d3e29acd759a6fb03b6389e
osiutino <osiutino@gmail.com>
2019-01-08 00:34:46 +0800

    chapter 01 - bigger image size

--- chapter01.cc
+++ chapter01.cc
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main () {
-  int nx = 200;
-  int ny = 100;
+  int nx = 800;
+  int ny = 600;
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
   for (int j = ny-1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
